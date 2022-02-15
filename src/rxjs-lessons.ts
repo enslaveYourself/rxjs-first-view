0601ae398a7de96173e6aaff2bfa37a7556b3afb
enslaveYourself <andrey.bezrukavyi@gmail.com>
2022-02-15 13:56:11 +0300

    rfjs take until subscription

--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -1,29 +1,60 @@
-import {debounceTime, distinctUntilChanged, map, Observable} from "rxjs";
+import {debounceTime, distinctUntilChanged, fromEvent, map, Observable, takeUntil} from "rxjs";
 
-const search$ = new Observable<Event>(observer => {
+/*const search$ = new Observable<Event>(observer => {
   const search = document.getElementById('search');
+  const stop = document.getElementById('stop');
 
-  search.addEventListener('input', event => {
-    observer.next(event);
-  });
-
-  if (!search) {
+  if (!search || !stop) {
     observer.error('Element does not exist on the page.)');
     return;
   }
 
-});
+  const onSearch = event => {
+    console.log(123);
+    checkSubscription();
+    observer.next(event);
+  };
+
+  const onStop = event => {
+    checkSubscription();
+    observer.complete();
+    clear();
+  }
+
+  search.addEventListener('input', onSearch);
+  stop.addEventListener('click', onStop);
+
+  const checkSubscription = () => {
+    if (observer.closed) {
+      clear();
+    }
+  }
+  const clear = () => {
+    search.removeEventListener('input', onSearch);
+    stop.removeEventListener('click', onSearch);
+  }
+});*/
+
+const search$: Observable<Event> = fromEvent<Event>(
+  document.getElementById('search'),
+  'input'
+);
+
+const stop$: Observable<Event> = fromEvent<Event>(
+  document.getElementById('stop'),
+  'click'
+);
 
 search$
   .pipe(
     map(event => {
       return (event.target as HTMLInputElement).value;
     }),
-    debounceTime(500),
+    debounceTime(700),
     map(value => value.length > 3 ? value : ''),
-    distinctUntilChanged())
+    distinctUntilChanged(),
+    takeUntil(stop$))
   .subscribe(value => {
+    console.log('Printing')
     console.log(value);
   });
-
-
