a7fb7e336252fc741b6046e5907a511bbefa01e3
enslaveYourself <andrey.bezrukavyi@gmail.com>
2022-02-16 08:53:10 +0300

    canvas drawing

---
+++ src/canvas.ts
@@ -0,0 +1,46 @@
+import {fromEvent, map, pairwise, switchMap, takeUntil} from "rxjs";
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+
+const cx = canvas.getContext('2d');
+
+cx.lineWidth = 4;
+
+interface Position {
+  x: number,
+  y: number
+}
+
+function drawLine([prev, next]: Position[]) {
+  cx.beginPath();
+  cx.moveTo(prev.x, prev.y);
+  cx.lineTo(next.x, next.y);
+  cx.stroke();
+}
+
+const mousemove$ = fromEvent<MouseEvent>(canvas, 'mousemove');
+const mousedown$ = fromEvent<MouseEvent>(canvas, 'mousedown');
+const mouseup$ = fromEvent<MouseEvent>(canvas, 'mouseup');
+const mouseout$ = fromEvent<MouseEvent>(canvas, 'mouseout');
+
+const points$ = mousemove$.pipe(
+  map<MouseEvent, Position>(({clientX, clientY}) => {
+    const {top, left} = canvas.getBoundingClientRect();
+    /*    console.log('Top - ' + top);
+        console.log('Left - ' + left);
+        console.log('ClientX - ' + clientX);
+        console.log('ClientY - ' + clientY);*/
+    return {
+      x: clientX - left,
+      y: clientY - top
+    }
+  }),
+  pairwise<Position>()
+);
+
+mousedown$.pipe(
+  switchMap(() => points$.pipe(
+    takeUntil(mouseout$),
+    takeUntil(mouseup$)
+  ))
+).subscribe(drawLine)
